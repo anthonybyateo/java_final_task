8fee54006ffa5c1d7c5c65f51ea5dabe3e89a419
anthonybyateo <anthony.by.ateo@gmail.com>
2019-05-19 06:38:33 +0300

    update

--- task_final/src/by/training/service/servicefactory/CreatorService.java
+++ task_final/src/by/training/service/servicefactory/CreatorService.java
@@ -1,25 +1,23 @@
 package by.training.service.servicefactory;
 
 import by.training.dao.DaoImplFactory.CreatorDao;
-import by.training.exception.PersistentException;
 import by.training.service.Service;
 import by.training.service.ServiceImpl;
 
 public class CreatorService {
-/*    private CreatorDao creatorDao;
+    private CreatorDao creatorDao;
 
-    public CreatorService(CreatorDao creatorDao) throws PersistentException {
+    public CreatorService(CreatorDao creatorDao) {
         this.creatorDao = creatorDao;
-    }*/
-
-    Service createService(ServiceImplFactory serviceFactory) throws PersistentException {
-        CreatorDao creatorDao = new CreatorDao();
-        ServiceImpl service;
-        service.setCreator(creatorDao);
-        return serviceFactory.createServiceImpl();
     }
 
-/*    public void close() {
+    Service createService(ServiceImplFactory serviceFactory) {
+        ServiceImpl service = serviceFactory.createServiceImpl();
+        service.setCreator(creatorDao);
+        return service;
+    }
+
+    public void close() {
         creatorDao.close();
-    }*/
+    }
 }
